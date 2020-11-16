6a1a67b5dcb8ea9192b853c64c0e2c2d2750e8a2
James Pace <jpace121@gmail.com>
2020-11-16 02:59:36 +0000

    Why are we displaying in a different thread than we're collection messages, if the two tasks as locked on each other?

--- src/j7s_mqtt_cpp/src/j7s-sub.cpp
+++ src/j7s_mqtt_cpp/src/j7s-sub.cpp
@@ -19,9 +19,6 @@
 #include <mqtt/async_client.h>
 #include <nlohmann/json.hpp>
 
-#include <thread>
-#include <mutex>
-
 #include <iostream>
 
 using json = nlohmann::json;
@@ -30,23 +27,20 @@ class J7sSubCallback : public mqtt::callback
 {
 public:
     J7sSubCallback(mqtt::async_client& client,
-                   blinkt_interface::Blinkt& blinkt,
-                   std::mutex& blinkt_mutex);
+                   blinkt_interface::Blinkt& blinkt);
 
 private:
     mqtt::async_client& _client;
     blinkt_interface::Blinkt& _blinkt;
-    std::mutex& _blinkt_mutex;
 
     void connected(const std::string& cause) override;
     void message_arrived(mqtt::const_message_ptr msg) override;
 };
 
 J7sSubCallback::J7sSubCallback(
-    mqtt::async_client & client, blinkt_interface::Blinkt & blinkt, std::mutex & blinkt_mutex) :
+    mqtt::async_client & client, blinkt_interface::Blinkt & blinkt) :
     _client(client),
-    _blinkt(blinkt),
-    _blinkt_mutex(blinkt_mutex){}
+    _blinkt(blinkt){}
 
 void J7sSubCallback::connected(const std::string&)
 {
@@ -70,45 +64,23 @@ void J7sSubCallback::message_arrived(mqtt::const_message_ptr msg)
     const auto index = payload["index"];
     const auto pixel = stringToPixel(payload["color"], payload["brightness"]);
 
-    std::cout << "Message arrived: Getting lock." << std::endl;
     {
-        std::lock_guard<std::mutex> lock(_blinkt_mutex);
-        std::cout << "Message arrived: Setting pixel." << std::endl;
         _blinkt.setPixel(index, pixel);
+        _blinkt.display();
     }
 
     return;
-}
-
-void displayFunc(const blinkt_interface::Blinkt & blinkt, std::mutex & blinkt_mutex, int sleep_time_ms)
-{
-    while(true)
-    {
-        {
-            std::cout << "Display. Getting lock." << std::endl;
-            std::lock_guard<std::mutex> lock(blinkt_mutex);
-            std::cout << "Display. Calling display." << std::endl;
-            blinkt.display();
-        }
-        std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time_ms));
-    }
 }
 
 int main(int, char **)
 {
     const std::string brokerAddr = getEnv("J7S_BROKER").value_or("localhost");
 
-    const double display_rate = std::stod(getEnv("J7S_DISPLAY_RATE").value_or("100"));
-    const int sleep_time_ms = static_cast<int>(floor(1.0 / display_rate) * 1e3);
-
     // Single blinkt object.
     blinkt_interface::Blinkt blinkt;
-    std::mutex blinkt_mutex;
-
-    std::thread display_thread(displayFunc, std::cref(blinkt), std::ref(blinkt_mutex), sleep_time_ms);
 
     mqtt::async_client client(brokerAddr, "");
-    J7sSubCallback callback(client, blinkt, blinkt_mutex);
+    J7sSubCallback callback(client, blinkt);
     client.set_callback(callback);
 
     client.connect();
